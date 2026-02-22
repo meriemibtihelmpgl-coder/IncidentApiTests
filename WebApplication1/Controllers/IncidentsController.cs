4b88136a66c1122ee76fa391c6b693eabd2d9f10
meriem lakhal <meriemlakhal12@gmail.com>
2026-02-22 21:41:01 +0100

    feat: add IncidentApi project

---
+++ WebApplication1/Controllers/IncidentsController.cs
@@ -0,0 +1,159 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class IncidentsController : Controller
+    {
+        private static readonly List<Incident> _incidents = new();
+        private static int _nextId = 1;
+        private static readonly string[] AllowedSeverities =
+         { "LOW", "MEDIUM", "HIGH", "CRITICAL" };
+        private static readonly string[] AllowedStatuses =
+         { "OPEN", "IN_PROGRESS", "RESOLVED" };
+        [HttpPost("create-incident")]
+        public IActionResult CreateIncident([FromBody] Incident incident)
+        {
+            if (!AllowedSeverities.Contains(incident.Severity))
+                return BadRequest("Severity invalide.");
+
+            incident.Id = _nextId++;
+            incident.Status = "OPEN";
+            incident.CreatedAt = DateTime.Now;
+            _incidents.Add(incident);
+            return Ok(incident);
+        }
+        [HttpGet("get-all")]
+        public IActionResult GetAllIncidents()
+        {
+            return Ok(_incidents);
+        }
+        [HttpGet("getbyid/{id}")]
+        public IActionResult GetIncidentById(int id)
+        {
+            try
+            {
+                var incident = _incidents.First(i => i.Id == id);
+                return Ok(incident);
+            }
+            catch (InvalidOperationException)
+            {
+                return NotFound();
+            }
+        }
+        [HttpPut("update-status/{id}")]
+        public IActionResult UpdateIncidentStatus(int id, [FromBody] string status)
+        {
+            var incident = _incidents.FirstOrDefault(i => i.Id == id);
+            if (incident == null)
+                return NotFound();
+
+            if (!AllowedStatuses.Contains(status))
+                return BadRequest("Status invalide.");
+
+            incident.Status = status;
+            return Ok(incident);
+        }
+        [HttpDelete("delete-incident/{id}")]
+        public IActionResult DeleteIncident(int id)
+        {
+            var incident = _incidents.FirstOrDefault(i => i.Id == id);
+
+            if (incident == null)
+                return NotFound();
+
+            if (incident.Severity == "CRITICAL" && incident.Status == "OPEN")
+                return BadRequest("Cannot delete an OPEN CRITICAL incident");
+
+            _incidents.Remove(incident);
+            return NoContent();
+        }
+
+        [HttpGet("filter-by-status")]
+        public IActionResult FilterByStatus([FromQuery] string status)
+        {
+            var result = _incidents
+                .Where(i => i.Status.Contains(status, StringComparison.OrdinalIgnoreCase))
+                .ToList();
+
+            return Ok(result);
+        }
+
+
+        [HttpGet("filter-by-severity")]
+        public IActionResult FilterBySeverity([FromQuery] string severity)
+        {
+            var result = _incidents
+                .Where(i => i.Severity.Contains(severity, StringComparison.OrdinalIgnoreCase))
+                .ToList();
+
+            return Ok(result);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ WebApplication1/Models/Incident.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class Incident
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(30,MinimumLength =3 ,ErrorMessage = "la taille du titre doit etre compris entre 3 et 30")]
+        public String Title { get; set; } = null!;
+        [Required]
+        [StringLength(200)]
+        public string Description { get; set; } = string.Empty;
+        [Required]
+        public string Severity { get; set; } = string.Empty;
+      //  [Required]
+        public string Status { get; set; } = string.Empty;
+      //  [Required]
+        public DateTime CreatedAt { get; set; }
+        
+
+
+    }
+}
